28faf0a7449a7bb1bfcc1cbd55cb23466f4f61fe
hasnaa0407 <162985128+hasnaa0407@users.noreply.github.com>
2024-03-24 18:09:34 +0100

    Create Hotel.java

---
+++ src/Hotel.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+
+public class Hotel{
+
+private static HashMap<Integer,Chambre> chambreMap = new HashMap<>();
+private static HashMap<Integer,User> userMap = new HashMap<>();
+private static HashMap<Integer,Reservation> reservationMap = new HashMap<>();
+
+
+//get Hashmap 
+	public static HashMap<Integer, Chambre> getChambreMap() {
+	return chambreMap;
+	}
+
+	public static HashMap<Integer, User> getUserMap() {
+	return userMap;
+	}
+
+	public static HashMap<Integer, Reservation> getReservationMap() {
+	return reservationMap;
+	}
+
+
+	static void AjouterChambreMap(Chambre chambre) {
+		chambreMap.put(chambre.getId(),chambre);
+	}
+	
+	static void AjouterUserMap(User user) {
+	     userMap.put(user.getId(), user);
+	}
+
+	static void AjouterReservationMap(Reservation reservation ) {
+	     reservationMap.put(reservation.getId(),reservation);
+	}
+	 
+	 
+	//modification sur les hashmap
+	static void SupprimerChambreMap(Chambre chambre) {
+		chambreMap.remove(chambre.getId());
+	}
+
+	static void SupprimerUserMap(User user) {
+		userMap.remove(user.getId());
+	}
+
+	static void SupprimerReservationMap(Reservation reservation) {
+		reservationMap.remove(reservation.getId());
+	}
+	
+	
+	
+	static void ModifierChambreMap(Chambre chambrenew) {
+		chambreMap.replace(chambrenew.getId(), chambrenew);
+	}
+	
+	static void ModifierReservationMap(int id,Reservation reservation) {
+		reservationMap.replace(id,reservation);
+	}
+	
+	void recherchechambredispo(String type) {
+		}
+
+
+	
+}
