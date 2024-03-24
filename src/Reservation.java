66c33d938e03357fff3f2d62aeffe3a1ba5fe0b2
Aoudia Nour Islam <154976644+NorIslam04@users.noreply.github.com>
2024-03-24 18:47:32 +0100

    Update Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,112 +1,111 @@
-package hotel;
-enum Etat {
-    accepted,
-    declined,
-    notyet,
-
-}
 public class Reservation {
 	
-		private int id;//pour hash map
-		private User user;//f la bdd 3ndy inteager id-user int    int l=id-integer, , user.id
-		private DateProgramme dateDebut;
-		private DateProgramme dateFin;
-		private TypeChambre type;
-		private Chambre chambre;
-		private Etat etat;//accepted wla declinee, null
-		
+		private int id;
+		private int idUser;
+		private String type;
+		private Date dateDebut;
+		private Date dateFin;
+		private int idChambre;
+		private String etat="en attente";// new reservation
 		private int nbOp=0;
-		static int nbid=0;
-
-	
-	public Reservation(int iduser,int idchambre,String dateDebut,TypeChambre type, String dateFin, Etat etat) {
-		this.dateDebut = DateProgramme.StringDateint(dateFin);
-		this.dateFin = DateProgramme.StringDateint(dateFin);
-		this.etat = etat;
-		this.type=type;
-		this.user=GestionMapBdd.RechercheuserParId(iduser);
-		this.chambre=GestionMapBdd.RechercheChambreParId(idchambre);//fhed les fct g pas encore geree les err
-	}
-	
-				
-			public int getId() {
-				return id;
-			}
-			public void setId(int id) {
-				this.id = id;
-			}
+		static int nb=0;
+		private boolean indb=false;
 		
-			public DateProgramme getDateDebut() {
-				return dateDebut;
-			}
-			public void setDateDebut(DateProgramme dateDebut) {
-				try {
-					DateProgramme.verifdatevalide(dateDebut);
-				this.dateDebut = dateDebut;}
-				catch(IllegalArgumentException e) {
-					throw e; /////////////////////////////////////
-				}
-			}
-			public DateProgramme getDateFin() {
-				return dateFin;
-			}
-			public void setDateFin(DateProgramme dateFin) {
-				try {
-			
-					DateProgramme.verifdatevalide(dateFin);
-				this.dateFin = dateFin;}
-				catch(IllegalArgumentException e) {
-					throw e; /////////////////////////////////////
-				}
-			}
-			public Etat getEtat() {
-				return etat;
-			}
-			public void setEtat(Etat etat) {
-				this.etat = etat;
-			}
-			public int getNbOp() {
-				return nbOp;
-			}
+		public Reservation(int id,int idUser, String type, Date dateDebut, Date dateFin, int idChambre,String etat) {
+			this.id = id;
+			this.idUser = idUser;
+			this.type = type;
+			this.dateDebut = dateDebut;
+			this.dateFin = dateFin;
+			this.idChambre = idChambre;
+			this.etat = etat;
+			nb++;
+		}
 
 
-			
 
-			public void setNbOp(int i) {
-				this.nbOp=i;
-				
-			}
-			//methode reservation pas encore acceptee 
-			//methode pour afficher toutes les reservation d une personne 
+		public int getId() {
+			return id;
+		}
 
+		public void setId(int id) {
+			this.id = id;
+		}
 
-			public Chambre getChambre() {
-				return chambre;
-			}
+		public int getIdUser() {
+			return idUser;
+		}
 
+		public void setIdUser(int idUser) {
+			this.idUser = idUser;
+		}
 
-			public void setChambre(Chambre chambre) {
-				this.chambre = chambre;
-			}
+		public String getType() {
+			return type;
+		}
 
+		public void setType(String type) {
+			this.type = type;
+		}
 
-			public void setUser(User user) {
-				this.user = user;
-			}
+		public Date getDateDebut() {
+			return dateDebut;
+		}
 
-			public User getUser() {
-				return this.user;
-			}
+		public void setDateDebut(Date dateDebut) {
+			this.dateDebut = dateDebut;
+		}
 
+		public Date getDateFin() {
+			return dateFin;
+		}
 
-			public TypeChambre getType() {
-				return type;
-			}
+		public void setDateFin(Date dateFin) {
+			this.dateFin = dateFin;
+		}
 
+		public int getIdChambre() {
+			return idChambre;
+		}
 
-			public void setType(TypeChambre type) {
-				this.type = type;
-			}
+		public void setIdChambre(int idChambre) {
+			this.idChambre = idChambre;
+		}
 
+		public String getEtat() {
+			return etat;
+		}
 
+		public void setEtat(String etat) {
+			this.etat = etat;
+		}
+
+		public int getNbOp() {
+			return nbOp;
+		}
+
+		public void setNbOp(int nbOp) {
+			this.nbOp = nbOp;
+		}
+
+		public static int getNb() {
+			return nb;
+		}
+
+		public static void setNb(int nb) {
+			Reservation.nb = nb;
+		}
+
+		public boolean isIndb() {
+			return indb;
+		}
+
+		public void setIndb(boolean indb) {
+			this.indb = indb;
+		}
+
+		
+
+	
+	
 }
