90a28d1f6e3df403492fe17ff80b09c5fcf20b3c
hasnaa0407 <162985128+hasnaa0407@users.noreply.github.com>
2024-03-24 18:23:34 +0100

    Update and rename Admin to Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,55 +1,68 @@
-package hotel;
-import java.util.Date;
 public class Admin {
-	private int id;
+
 	private String name;
 	private String password;
-	
-	
+	private int id;
 	private int nbOp=0;
-	static int nbid=0;//id pour hash map
+	static int nb=0;
+
 	
-	
-	public Admin(int id,String name, String password) {
+	public Admin(String name, String password, int id) {
 		this.name = name;
 		this.password = password;
 		this.id = id;
-
+		nb++;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 
 	public String getPassword() {
 		return password;
 	}
 
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 
 	public int getId() {
 		return id;
 	}
 
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 
 	public int getNbOp() {
 		return nbOp;
 	}
 
+
 	public void setNbOp(int nbOp) {
 		this.nbOp = nbOp;
 	}
-	
-	
-	
 
+
+	public static int getNb() {
+		return nb;
+	}
+
+
+	public static void setNb(int nb) {
+		Admin.nb = nb;
+	}
+
+	
 }
