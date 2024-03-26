09be237a68c609215f33b65cc78d9672eacfa51c
Aoudia Nour Islam <154976644+NorIslam04@users.noreply.github.com>
2024-03-26 18:57:54 +0100

    Update Reservation.java

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,3 +1,35 @@
+public enum EtatReservation {
+    ACCEPTER,
+    DECLINER,
+    EN_ATTENTE;
+
+    public static EtatReservation toEtatReservation(String str) {
+        switch (str) {
+            case "ACCEPTER":
+                return ACCEPTER;
+            case "DECLINER":
+                return DECLINER;
+            case "EN_ATTEND":
+                return EN_ATTENTE;
+            default:
+                throw new IllegalStateException("État de réservation inconnu : " + str);
+        }
+    }
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case ACCEPTER:
+                return "ACCEPTER";
+            case DECLINER:
+                return "DECLINER";
+            case EN_ATTENTE:
+                return "EN_ATTEND";
+            default:
+                throw new IllegalArgumentException("État de réservation invalide : " + this);
+        }
+    }
+}
 public class Reservation {
 
 	private int id;//de hash map
