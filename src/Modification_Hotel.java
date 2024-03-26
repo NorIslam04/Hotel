691cf1fcde30eb14043e963a8d2d76c5a50cd033
NorIslam04 <islamaoudi18@gmail.com>
2024-03-27 00:11:05 +0100

    class Modification_hotel

---
+++ src/Modification_Hotel.java
@@ -0,0 +1,68 @@
+public class Modification_Hotel<T,O> {
+
+    private int id;// de user,chambre,reservation
+    private T objet;// user,chambre,reservation
+    private O operation;//AJOUT,SUPPRESSION,MODIFICATION
+    private static int nb=0;
+
+    /*
+     * le but de cette class dans le convertion de hashMap -> BDD 
+     * utilison comme premery-key le id et objet pour recuperer l'instance modifier dans hashMap de l'hotel
+     * l'attribue operation pour separer les modification dans la BDD
+     */
+
+
+    public Modification_Hotel(int id, T objet, O operation) {
+        this.id = id;
+        this.objet = objet;
+        this.operation = operation;
+        nb++;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public T getObjet() {
+        return objet;
+    }
+    public void setObjet(T objet) {
+        this.objet = objet;
+    }
+    public O getOperation() {
+        return operation;
+    }
+    public void setOperation(O operation) {
+        this.operation = operation;
+    }
+
+
+
+    public static int getNb() {
+        return nb;
+    }
+
+
+    public static void setNb(int nb) {
+        Modification_Hotel.nb = nb;
+    }
+
+    
+    /*//tous les operation......
+     * if(id=1&&objet user){
+     * 
+     * }if(id=1&&objet chambre){}
+     * 
+     * 
+
+     * 
+     */
+
+
+
+
+    
+}
