f9a796da1c068b4f55e219862436c4a3571f8331
NorIslam04 <islamaoudi18@gmail.com>
2024-03-26 18:10:54 +0100

    ajt des panel dans inetrefaces

--- src/Signeininterface.java
+++ src/Signeininterface.java
@@ -138,10 +138,23 @@ public class Signeininterface extends javax.swing.JFrame {
             System.exit(0);
         }
     }    
-    private void submitbtnActionPerformed(java.awt.event.ActionEvent evt) {                                          
-        chambreinterface ch = new chambreinterface();
-        ch.setVisible(true);
-        this.hide();
+    private void submitbtnActionPerformed(java.awt.event.ActionEvent evt) {   
+        if(usertext.getText().equals("") || pwdtext.getText().equals("") || mailtext.getText().equals("")){
+            JOptionPane.showMessageDialog(frame,
+            "Remplir le champ de User et Password et Email",
+            "ERROR",
+            JOptionPane.ERROR_MESSAGE); 
+        }else{
+
+            JOptionPane.showMessageDialog(frame,
+            "Votre compte a été créé avec succès ",
+            "WELCOME",
+            JOptionPane.INFORMATION_MESSAGE);
+            //ajoute dans hashMap users
+            chambreinterface ch = new chambreinterface();
+            ch.setVisible(true);
+            this.hide();
+        }
     }                                       
 
     public static void main(String args[]) {
